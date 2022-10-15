c6c0d92c675e39cf01a9589f5e2c227c8303b518
Premmakwana <95080888+Premmakwana@users.noreply.github.com>
2022-10-15 17:54:16 +0530

    Initial Structure

---
+++ src/main/java/com/prem/pizzadeliveryapp/PizzaDeliveryAppApplication.java
@@ -0,0 +1,13 @@
+package com.prem.pizzadeliveryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaDeliveryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaDeliveryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prem/pizzadeliveryapp/PizzaDeliveryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prem.pizzadeliveryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaDeliveryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
